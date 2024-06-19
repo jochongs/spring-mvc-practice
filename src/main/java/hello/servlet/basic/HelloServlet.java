c4ace7abc954ff441d04da944c5f39d8d2d60544
jochongs <joch2712@naver.com>
2024-06-19 14:38:45 +0900

    feat: Create GET /hello API

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,21 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String username = req.getParameter("name");
+
+        res.setContentType("text/pain");
+        res.setCharacterEncoding("utf-8");
+        res.getWriter().write("Hello" + username);
+    }
+}
