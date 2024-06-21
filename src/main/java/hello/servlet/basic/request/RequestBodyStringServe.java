67c3e1a4a7f705e815f36047ef2646c5bd616b75
jochongs <joch2712@naver.com>
2024-06-21 21:17:46 +0900

    Create request body

--- src/main/java/hello/servlet/basic/request/RequestBodyStringServe.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServe.java
@@ -8,18 +8,34 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.util.StreamUtils;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 @WebServlet(name = "requestBodyStringServe", urlPatterns = "/request-body-string")
 public class RequestBodyStringServe extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ServletInputStream inputStream = req.getInputStream();
+        String name = req.getParameter("name");
 
-        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("name = " + name);
+//        System.out.println("===========");
+//        System.out.println(req.getParameter("name"));
+//
+//        ServletInputStream inputStream = req.getInputStream();
 
-        System.out.println("messageBody = " + messageBody);
+//        System.out.println("===========");
+//        String body = new BufferedReader(new InputStreamReader(inputStream))
+//                .lines()
+//                .collect(Collectors.joining("\n"));
+//        System.out.println(body);
+
+//        System.out.println("===========");
+//        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+//        System.out.println(messageBody);
     }
 }
