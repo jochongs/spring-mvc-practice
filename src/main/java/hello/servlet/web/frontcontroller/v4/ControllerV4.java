eaa950a2b42ac4a36eeb0ce82c7a8a625bd45446
jochongs <joch2712@naver.com>
2024-07-03 14:38:52 +0900

    Create code comment

--- src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -4,5 +4,11 @@ import java.util.Map;
 
 public interface ControllerV4 {
 
+    /**
+     * 4번째 버전 controller 인터페이스의 메인 실행 메서드
+     * @param paramMap
+     * @param model
+     * @return
+     */
     String process(Map<String, String> paramMap, Map<String, Object> model);
 }
