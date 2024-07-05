267d694c5c6165219feabfa6031ba62433d80e76
jochongs <joch2712@naver.com>
2024-07-05 13:46:07 +0900

    Create spring member controller v3

--- src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -35,7 +35,7 @@ public class SpringMemberControllerV2 {
         return mv;
     }
 
-    @RequestMapping("/")
+    @RequestMapping
     public ModelAndView members() {
         List<Member> members = memberRepository.findAll();
 
